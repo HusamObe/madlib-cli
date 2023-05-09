4de141c8a654604c561fd84cb3914837ac99a730
Husam Obeidat <husamobe1991@gmail.com>
2023-05-09 08:45:38 +0000

    finish req.

--- madlib.py
+++ madlib.py
@@ -1,22 +1,50 @@
 import re
 
+
+def intro():
+    print(
+        "Welcome to the Madlib program! This is an interactive game where you will provide various words to fill in the blanks of a pre-written story. The program will prompt you to enter specific types of words, such as nouns, verbs, and adjectives, which will then be inserted into the story to create a unique and silly Madlib."
+    )
+
+
+def prompts(lst):
+    input_arr = []
+    for element in lst:
+        user_input = input(f"enter a {element}")
+        input_arr.append(user_input)
+    return input_arr
+
+
 def read_template(path):
     try:
-        print("Welcome to the Madlib program! This is an interactive game where you will provide various words to fill in the blanks of a pre-written story. The program will prompt you to enter specific types of words, such as nouns, verbs, and adjectives, which will then be inserted into the story to create a unique and silly Madlib.")
-        with open(path, 'r') as file:
-            return file.read()
+        with open(path, "r") as file:
+            content = file.read()
+            return content
     except FileNotFoundError:
         raise FileNotFoundError(f"File not found at path: {path}")
 
+
 def parse_template(template):
-    parts = template
-    variables = re.findall(r'{([^}]+)}', template)
-    stripped = re.sub("{[^}]+}", "{}", parts)
+    variables = re.findall(r"{([^}]+)}", template)
+    stripped = re.sub("{[^}]+}", "{}", template)
     return stripped, tuple(variables)
 
+
 def merge(template, parts):
-    for part in parts:
-        template = template.replace("{}", str(part), 1)
+    template = template.format(*parts)
     return template
 
-read_template("./assets/dark_and_stormy_night_template.txt")
+
+def new_file(merged_template):
+    with open("./assets/new_file.txt", "w") as f:
+        f.write(merged_template)
+
+
+if __name__ == "__main__":
+    intro()
+    returned_content = read_template("./assets/dark_and_stormy_night_template.txt")
+    stripped, parts = parse_template(returned_content)
+    user_prompts = prompts(parts)
+    merged_txt = merge(stripped, user_prompts)
+    print(merged_txt)
+    new_file(merged_txt)
